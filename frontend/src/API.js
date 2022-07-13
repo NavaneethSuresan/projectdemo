feea0e5edea3501e45d7ffd8138b660f3fec58ea
NavaneethSuresan <navaneeth@techis.io>
2022-07-13 15:24:42 +0530

    done

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 const LOGIN_USER_KEY = 'WD_FORUM_LOGIN_USER_KEY';
 
-var baseURL;
-if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
-    baseURL = process.env.REACT_APP_API_BASE_URL;
-} else {
-    baseURL = 'http://127.0.0.1:8000';
-}
+// var baseURL;
+// if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
+//     baseURL = process.env.REACT_APP_API_BASE_URL;
+// } else {
+//     baseURL = 'http://127.0.0.1:8000';
+// }
+baseURL = 'https://projectdemo-backend.herokuapp.com/';
 
 const api = axios.create({
     baseURL: baseURL,
